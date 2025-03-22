bc4bc39b0d862df92e192d5f7ba3cb72a26e94e2
박은경 <dmsrud1501222@naver.com>
2025-03-22 21:02:41 +0900

    test(option): 상품 상세페이지 옵션 설정 관련 E2E 테스트 코드 작성

---
+++ tests/option.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from "@playwright/test";
+import { SearchPage } from "../pom/searchPage";
+import { OptionPage } from "../pom/optionPage";
+
+test("OPTION_001: 상품 옵션 변경 및 변경 사항 반영 확인 ", async ({ page }) => {
+  const searchPage = new SearchPage(page);
+  const optionPage = new OptionPage(page);
+
+  await searchPage.goto();
+  await searchPage.searchItem("기본명함");
+  await searchPage.searchResults.waitFor({
+    state: "visible",
+  });
+  await searchPage.searchResultItem.click();
+  const selectedText = await optionPage.selectOption("용지", "코팅스노우 219g");
+  expect(selectedText).toContain("코팅스노우 219g");
+});

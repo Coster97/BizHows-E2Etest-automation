de79e5daba7386901430cd39bf32cc3dfd8ca954
박은경 <dmsrud1501222@naver.com>
2025-03-22 23:51:47 +0900

    test(design): 디자인 진행 방식 선택 및 파일 업로드 삽입 구현

---
+++ tests/design.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../pom/loginPage";
+import { DesignPage } from "../pom/designPage";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+// 구글 계정
+const existingEmail = process.env.GOOGLE_EXISTINGEMAIL as string;
+const pass = process.env.GOOGLE_PASSWORD as string;
+
+test("test", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  const designPage = new DesignPage(page);
+
+  await loginPage.goto();
+  await loginPage.googleLogin(existingEmail, pass);
+  await loginPage.loginButton.waitFor({
+    state: "detached",
+  });
+
+  await page.goto(
+    "https://www.bizhows.com/v/option?code1=5000&code2=200&code3=3501&mock=5000_200_3501_7&from=megamenu&selectedOptionList=119357%2C115832%2C115754%2C131903%2C115701%2C119341%2C115774%2C115721%2C115723%2C115752%2C115726%2C115728%2C115727%2C115738%2C115748%2C115750",
+    { waitUntil: "commit" }
+  );
+  await designPage.selectDesignMethod();
+  await designPage.selectDesignFile();
+  await page.waitForURL("https://www.bizhows.com/v/step3-cart", {
+    waitUntil: "load",
+  });
+
+  await expect(designPage.cartTitle).toBeVisible();
+});
